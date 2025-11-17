67941b34154f435900564de4bbaa5d2237313cdc
bruno angelocci <brunoangelocci@gmail.com>
2025-11-17 09:16:35 -0300

    script criacao banco xampp

--- script_pic_db.sql
+++ script_pic_db.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE P2DB;
-USE P2DB;
+CREATE DATABASE IF NOT EXISTS  PICDB;
+USE PICDB;
 
-CREATE TABLE Users(
+CREATE IF NOT EXISTS TABLE Users(
     id int auto_increment primary key,
     email varchar(100) unique,
     password varchar(100)
