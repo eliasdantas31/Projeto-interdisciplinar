5363efa83599712a75afb1a306630a50b69aa614
bruno angelocci <brunoangelocci@gmail.com>
2025-11-23 13:46:24 -0300

    criado tabela tables e orders

--- script_pic_db.sql
+++ script_pic_db.sql
@@ -19,7 +19,6 @@ CREATE TABLE IF NOT EXISTS CategoryItem (
     price FLOAT NOT NULL,
     name VARCHAR(100) UNIQUE NOT NULL,
 
-
     CONSTRAINT fk_categoryitem_category
         FOREIGN KEY (categoryId)
         REFERENCES Category(id)
@@ -33,10 +32,62 @@ CREATE TABLE IF NOT EXISTS CategoryAdds (
     name VARCHAR(100) UNIQUE NOT NULL,
     price FLOAT NOT NULL,
 
-
-    CONSTRAINT fk_categoryitem_category
+    CONSTRAINT fk_categoryadds_category
         FOREIGN KEY (categoryId)
         REFERENCES Category(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Tables (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    num INT,               
+    name VARCHAR(100)      
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tableId INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_orders_table
+        FOREIGN KEY (tableId)
+        REFERENCES Tables(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS OrderItems (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    orderId INT NOT NULL,
+    itemId INT NOT NULL, 
+    quantity INT NOT NULL,
+    observation VARCHAR(255), 
+
+    CONSTRAINT fk_orderitems_order
+        FOREIGN KEY (orderId)
+        REFERENCES Orders(id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_orderitems_item
+        FOREIGN KEY (itemId)
+        REFERENCES CategoryItem(id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS OrderItemAdds (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    orderItemId INT NOT NULL,
+    addId INT NOT NULL,  
+    quantity INT NOT NULL DEFAULT 1,
+
+    CONSTRAINT fk_itemadds_orderitem
+        FOREIGN KEY (orderItemId)
+        REFERENCES OrderItems(id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_itemadds_add
+        FOREIGN KEY (addId)
+        REFERENCES CategoryAdds(id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
